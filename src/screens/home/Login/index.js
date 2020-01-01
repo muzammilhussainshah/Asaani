f2122caba31edffdbb59146cc72726c651463e42
haideralishah <headeralishah@gmail.com>
2020-01-01 21:49:19 +0500

    project sytup

---
+++ src/screens/home/Login/index.js
@@ -0,0 +1,88 @@
+import React, { useReducer } from 'react';
+import { View, StyleSheet, ScrollView, Text, Image, KeyboardAvoidingView, Dimensions, ActivityIndicator } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
+import { bindActionCreators } from 'redux';
+import { signInWithGoogle, login } from '../../../store/action/action'
+import { connect } from 'react-redux';
+
+// GoogleSignin.configure({
+//     webClientId: '433343540518-e710u0d5bef1sp5r0oloavaniqnumcm4.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+// });
+
+let { height, width } = Dimensions.get('window');
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    };
+    render() {
+        const { fields, loading } = this.state
+        return (
+            <View style={{flex:1,backgroundColor:"red"}}>
+           
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FDB8B0',
+        alignItems: 'center',
+        justifyContent: 'center',
+        // marginTop: 20
+    },
+    loading: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: "center",
+        // position: 'absolute'
+    },
+    Heading: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        // flex: 1,
+        marginTop: 5,
+        marginBottom: 5,
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#2A2D3A',
+        textAlign: 'center'
+    },
+    button: {
+        width: '100%',
+        alignItems: 'center',
+        marginTop: 10,
+        opacity: 1
+    },
+    imageView: {
+        width: width,
+        height: height / 2.6,
+        alignItems: "center",
+        justifyContent: 'flex-end'
+    },
+    signupimage: {
+        height: '90%',
+        width: '90%',
+        // marginTop: 30,
+    },
+})
+
+
+function mapStateToProps(states) {
+    return ({
+    })
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            signInWithGoogle, login
+        }, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
