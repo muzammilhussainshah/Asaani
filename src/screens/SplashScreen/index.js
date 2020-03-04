088bd4cd501c3b3710460554151861938437838a
haideralishah <headeralishah@gmail.com>
2020-03-04 18:13:31 +0500

    Main Opening screen, splash screen. Neche woi text and loader lagengy bs white color me

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image, Dimensions, ActivityIndicator } from 'react-native';
+import { View, StyleSheet, Text, Image, Dimensions, ActivityIndicator,ImageBackground } from 'react-native';
 import { getData, } from '../../store/action/action'
 import { connect } from 'react-redux';
 class SplashScreen extends React.Component {
@@ -14,24 +14,29 @@ class SplashScreen extends React.Component {
     }
     render() {
         return (
-            <View style={{ flex: 1, backgroundColor: "#fff", justifyContent: "center", alignItems: "center" }}>
-                <Image
-                    style={{ width: "50%", height: "30%",position:"absolute",zIndex:1 }}
-                    resizeMode="contain"
-                    source={require('../../../src/assets/logocopy.png')}
-                />
-                <View style={{marginTop: "100%",}}>
-                    <ActivityIndicator size="large" color="#0C4F7A" />
+            <ImageBackground source={require("../../assets/splash.jpg")}
+                style={{ width: '100%', height: '100%' }}>
+
+                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                    {/* <Image
+                        style={{ width: "50%", height: "30%", position: "absolute", zIndex: 1 }}
+                        resizeMode="contain"
+                        source={require('../../../src/assets/logocopy.png')}
+                    /> */}
+                    <View style={{ marginTop: "100%", }}>
+                        <ActivityIndicator size="large" color="#fff" />
+                    </View>
+                    <View style={{ marginTop: "15%", width: "100%", }}>
+                        <Text style={{
+                            color: "#fff", textAlign: "center", fontWeight: "bold", width: "100%",
+                            fontFamily: 'Verdana-Bold',
+                        }}>
+                            {'Online service to everyone'}
+                        </Text>
+                    </View>
                 </View>
-                <View style={{ marginTop: "15%", width: "100%", }}>
-                    <Text style={{
-                        color: "#0C4F7A", textAlign: "center", fontWeight: "bold", width: "100%",
-                        fontFamily: 'Verdana-Bold',
-                    }}>
-                        {'Online service to everyone'}
-                    </Text>
-                </View>
-            </View>
+            </ImageBackground>
+
         );
     }
 }
